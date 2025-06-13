4ff5acb56d6f6aadb1bd762da4b108093285fb82
Liu <liupinzh@amazon.com>
2025-06-13 18:30:21 +0800

    Fix GitHub Pages routing and resource paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {
   Box,
   ChakraProvider
 } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { theme } from './theme';
 import Home from './pages/Home';
 import Reading from './pages/Reading';
